e041a396411cd11e6bd36c64676022a2d6e071eb
MirnaCardona <69806001+MirnaCardona@users.noreply.github.com>
2020-11-27 20:49:36 -0600

    Update Proy1.c

--- Proy1.c
+++ Proy1.c
@@ -57,14 +57,14 @@ int esMBR(char *base){
 
 int LeeDirArch(int dr, int i, short int *edr)
 {
- char arc[8], ext[3];//,arc2[8], ext2[3],arc3[8], ext3[3];
+ char arc[8], ext[3], temp[2];//,arc2[8], ext2[3],arc3[8], ext3[3];
   int j, temp=0;
 
   j=dr+i;
   //i=0;
   while(temp<=*edr+32)
   {
-    memset(arc,0,8);
+   // memset(arc,0,8);
       strcpy(arc, &map[j+0x00]); // nombre del archivo
         arc[8]='\0';
       strcpy(ext, &map[j+ 0x08]); // extencion del archivo
@@ -73,7 +73,7 @@ int LeeDirArch(int dr, int i, short int *edr)
    short int *cluster = (short int *)&map[j+0x1a];
    short int *tama = (short int *)&map[j+0x1c];
                      
-  printf("Archivo   %s   .%s    ,%d   ,%d   ,%d\n", arc, ext, *tipo, *cluster, *tama);
+  printf("Archivo %s  %s   .%s    ,%d   ,%d   ,%d\n", temp, arc, ext, *tipo, *cluster, *tama);
   j=j+32;
     temp=temp+32;
 }
