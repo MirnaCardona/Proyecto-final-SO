5e87bf860454de2346be9f229a5670e61db24961
MirnaCardona <69806001+MirnaCardona@users.noreply.github.com>
2020-11-27 20:48:15 -0600

    Update Proy1.c

--- Proy1.c
+++ Proy1.c
@@ -64,6 +64,7 @@ int LeeDirArch(int dr, int i, short int *edr)
   //i=0;
   while(temp<=*edr+32)
   {
+    memset(arc,0,8);
       strcpy(arc, &map[j+0x00]); // nombre del archivo
         arc[8]='\0';
       strcpy(ext, &map[j+ 0x08]); // extencion del archivo
